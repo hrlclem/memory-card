650115db123026d6d522004bb433e93f261488a5
Clement <perso@clementhurel.fr>
2022-09-15 21:46:23 +0900

    fixing css

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,15 @@ const Header = (props) => {
 
     return(
         <div className="header">
-            <div className="title">Kanji Memory Game</div>
-            <div className="instructions">Get points by clicking on a Kanji. Be careful, click on it only once!</div>
-            <div className="score">Current score: {props.score}</div>
-            <div className="highScore">Highest score: {props.highScore}</div>
-            <div className="reset">{props.reset}</div>
-        </div>
+            <div className="leftSide">
+                <div className="titleHeader">Kanji Memory Game</div>
+                <div className="instructions">Get points by clicking on a Kanji. Be careful, click on it only once!</div>
+            </div>   
+            <div className="rightSide">
+                <div className="score">Current score: {props.score}</div>
+                <div className="highScore">Highest score: {props.highScore}</div>
+            </div>     
+       </div>
     )
 }
 
